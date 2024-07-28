23b8f9689a1ea0ecfc0ab0b89429ffb5a824adb1
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-07-28 17:44:32 -0500

    Add useLoaderData to Retrive Dummy Contact Info & Resolve TS Errors

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,17 +1,27 @@
-import { Form } from "@remix-run/react";
-import type { FunctionComponent } from "react";
+import { json } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import { ContactRecord, getContact } from "../data";
 
-import type { ContactRecord } from "../data";
+import type { FunctionComponent } from "react";
+import type { LoaderFunctionArgs } from "@remix-run/node";
+
+import invariant from "tiny-invariant";
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  // invariant asserts truthiness to make typescript stop yelling,
+  // as well as throw custom error messages when you anticipate errors
+  invariant(params.contactId, "Missing contactId param");
+  const contact = await getContact(params.contactId);
+
+  // uncomment this line below and see what it solves; it kills two birds
+  // with one stone by accounting for a null value and sending a web response
+  if (!contact) throw new Response("Not Found", { status: 404 });
+
+  return json({ contact });
+};
 
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placekitten.com/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+  const { contact } = useLoaderData<typeof loader>();
 
   return (
     <div id="contact">
